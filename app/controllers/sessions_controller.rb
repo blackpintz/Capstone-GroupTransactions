c453c8d3b4415149cfbe9b4680175f8c1d8cfe24
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-07-28 15:02:57 +0000

    added links to the user profile page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,18 @@
 class SessionsController < ApplicationController
-    def new
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully.'
+      redirect_to user_path(user)
+    else
+      flash.now[:alert] = 'Wrong username.'
+      render 'new'
     end
-    
-    def create
-        user = User.find_by(username: params[:session][:username])
-        if user
-          session[:user_id] = user.id
-          flash[:notice] = 'Logged in successfully.'
-          redirect_to user_path(user)
-        else
-          flash.now[:alert] = 'Wrong username.'
-          render 'new'
-        end
-    end
-    
+  end
+
   def destroy
     session[:user_id] = nil
     flash[:notice] = 'You are logged out.'
