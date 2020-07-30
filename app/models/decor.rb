c49095a991be84b7f1e3de777cbeae110446f54e
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-07-30 13:42:41 +0000

    built associations between decor and category models.

--- app/models/decor.rb
+++ app/models/decor.rb
@@ -2,6 +2,6 @@ class Decor < ApplicationRecord
   belongs_to :user
   has_many :decor_categories
   has_many :categories, through: :decor_categories
-  
+
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
