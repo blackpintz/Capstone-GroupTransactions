4bb7e1d019a973c2bfbe6e7af9a1dc7d2191b62a
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-07-30 10:46:59 +0000

    Set up model,controller and routes for the Decortable.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_730_092_123) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2020_07_30_103901) do
+
+  create_table "decors", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "price", precision: 10, scale: 2
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_decors_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "decors", "users"
 end
