0c422b7d57b10ca8162686473dbafa01e52fa00e
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-07-31 13:44:01 +0000

    Fix the select field in the decor form.

--- app/models/decor.rb
+++ app/models/decor.rb
@@ -1,6 +1,6 @@
 class Decor < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  belongs_to :category, optional: true
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   scope :with_no_category, -> { where(category_id: nil) }
