ce54a8b80272cfaf1a8689068cda05d7009ddc5e
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-08-01 11:36:19 +0000

    Fixed the design look of the error msgs on the ui.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,17 @@ class UsersController < ApplicationController
   before_action :require_login, only: %i[show]
   before_action :require_user, only: [:show]
   before_action :not_logged_in?, only: [:new]
-  def new; end
+
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
-    session[:user_id] = @user.id
-    flash[:notice] = "Welcome #{@user.username}"
-    redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome #{@user.username}"
+      redirect_to user_path(@user)
     else
       render 'new'
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :decors
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :icon, presence: true
   scope :alphabetical_order, -> { order(name: :asc) }
 end
