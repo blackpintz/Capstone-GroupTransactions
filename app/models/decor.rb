bfd15bf89cc81abba229eccc28656e2b1e4e52f3
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-08-03 09:17:31 +0000

    add edit and delete features for the decor model.

--- app/models/decor.rb
+++ app/models/decor.rb
@@ -1,8 +1,8 @@
 class Decor < ApplicationRecord
   belongs_to :user
   belongs_to :category, optional: true
-
   validates :name, presence: true
+  validates :name, uniqueness: { scope: :user_id, message: 'already exists!', case_sensitive: false }
   validates :price, presence: true
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
