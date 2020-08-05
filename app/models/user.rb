c27b4b06be59d5b0cb83f6c8a335c4c606ac605c
Ubuntu <ubuntu@ip-172-31-20-66.us-east-2.compute.internal>
2020-08-05 18:49:28 +0000

    fixed unlisted page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   validates :avatar, content_type: { in: ['image/png', 'image/jpg', 'image/jpeg'], message: 'should be a jpg, jpeg or png.' }
   validates :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
+
+  def decors_with_no_category(decors)
+    decors.select { |decor| decor.categories.empty? }
+  end
 end
